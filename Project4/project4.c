aae1365098c9319f0ed912e2ec287506a83bfb5e
AlexCMD-DEV <nomercyalex14@gmail.com>
2024-10-23 11:18:45 +0500

    Modified code in project4.c and create new txt file

---
+++ Project4/project4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // This is a comment
+  printf("Hello world!/n/n");  // This is a comment
+
+  /* The code below will print the words I am learning C!
+to the screen, and it is amazing */
+  printf("I am learning C.");
+  return 0;
+}
